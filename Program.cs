8d5349d6db48d8d571fbde1d514c00cfa9e44c7b
Nofal <nofalreal62@gmail.com>
2025-04-10 19:03:16 +0400

    added the C# template

--- Program.cs
+++ Program.cs
@@ -2,3 +2,14 @@
 using System.Linq;
 using System.Text;
 using System;
+
+namespace Nogal_Git
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
