e67f5b61cac3b8ba114c4b4ec907d4b651a1f136
Nofal <nofalreal62@gmail.com>
2025-04-12 15:05:27 +0400

    remoced the contructer bc no point of having them

--- Info.cs
+++ Info.cs
@@ -5,14 +5,6 @@ namespace Git_test
         public static double a;
         public  static double b;
         public static double c;
-
-        public Info(double A, double B, double C)
-        {
-            a = A;
-            b = B;
-            c = C;
-        }
-    
     }
 }
 
--- obj/Debug/net10.0/Git_test.AssemblyInfo.cs
+++ obj/Debug/net10.0/Git_test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Git_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785eaa6b52122b31abda1ce863a21f26f4a7e4c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c5597806e83660bcec398e2982931ce42a7e58f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Git_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Git_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
